0587a30f41dcd7177b159b32c0e33509073e91d6
john-almaraz <125034147+john-almaraz@users.noreply.github.com>
2023-04-26 06:58:50 +0200

    .

---
+++ EscuelaJava/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"prueba.semana1.model"}];updateSearchResults();
---
+++ EscuelaJava/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"prueba.semana1.model","l":"Vehiculo"}];updateSearchResults();
